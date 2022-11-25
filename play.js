540a5d7acad4135ff4b6c3194594375a220d6ef0
Ethan <ethan.steip@gmail.com>
2022-11-25 00:31:22 -0500

    added ES6 module export and require sytax for connect module

--- play.js
+++ play.js
@@ -1,27 +1,16 @@
+/*
+* Snake Game - building a snake game using TCP and Nodes "Net" library.
+*/
+ 
+ 
+/*
+*
+* Function Name - and a brief description of what it does
+*
+* @param {string} argument name - information it holds
+* @param {string} argument name - information it holds
+*
+*/
+
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "192.168.0.41",
-    port: "50541"
-  });
-
-  // When connecting to the local snake server, print a message to the client.
-  conn.on('data', (data) => {
-    console.log(data.toString());
-    conn.end();
-  });
-  // When disconnecting from the server, print a message to the client.
-  // client.on('end', () => {
-  //   console.log('disconnected from server');
-  // });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
+const { conn } = require('./client.js');
